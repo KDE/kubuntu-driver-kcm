0b502de9b93de83972eaa90c382448403026bc5b
Rohan Garg <rohan@kde.org>
2014-04-23 13:43:41 +0200

    Add a apport hook to provide more debug info when reporting bugs

---
+++ apport/kubuntu-driver-manager.py
@@ -0,0 +1,10 @@
+# apport package hook for ubuntu-drivers-common
+# (c) 2012 Canonical Ltdt.
+# Author: Martin Pitt <martin.pitt@ubuntu.com>
+# (c) 2014 Rohan Garg <rohangarg@kubuntu.org>
+
+import apport.hookutils
+
+def add_info(report, ui):
+    report['UbuntuDriversDebug'] = apport.hookutils.command_output(['ubuntu-drivers', 'debug'])
+    report['UbuntuDriversList']  = apport.hookutils.command_output(['ubuntu-drivers', 'devices'])
